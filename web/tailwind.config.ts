5eb4067ea73047a4c540a51fd9864ac78425b731
cesarhdzmorado <cesar.hernandezm@outlook.com>
2025-06-15 21:26:40 +0000

    added changes to the tailwind form

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -3,15 +3,16 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'), // We will install this next
+    // This ensures the typography plugin is active
+    require('@tailwindcss/typography'),
   ],
 }
 export default config
