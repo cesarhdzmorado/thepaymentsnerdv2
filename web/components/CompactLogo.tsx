4b7bfa47951672dfb2cf4694aa010391dd4ff4dd
cesarhdzmorado <cesar.hernandezm@outlook.com>
2025-12-22 16:39:38 +0000

    Update CompactLogo.tsx

--- web/components/CompactLogo.tsx
+++ web/components/CompactLogo.tsx
@@ -1,9 +1,9 @@
 // web/components/CompactLogo.tsx
 export function CompactLogo() {
   return (
-    <div className="select-none text-xl font-bold tracking-tight">
+    <div className="select-none text-xl font-bold tracking-tight transition-colors">
       <span className="text-slate-500 dark:text-slate-400">/</span>
-      <span className="text-slate-900 dark:text-slate-100">
+      <span className="text-slate-900 dark:text-slate-100 hover:text-slate-700 dark:hover:text-slate-50">
         thepaymentsnerd
       </span>
     </div>
